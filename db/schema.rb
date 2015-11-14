1f4df2fe95bc51416a890a4b09438b4a681f5d07
PSkoczylas <piotr.skoczylas93@gmail.com>
2015-11-14 19:57:50 +0100

    Some small change files

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151008233711) do
+ActiveRecord::Schema.define(version: 20151114183043) do
 
   create_table "users", force: :cascade do |t|
     t.string  "first_name"
@@ -22,5 +22,7 @@ ActiveRecord::Schema.define(version: 20151008233711) do
     t.integer "id_student"
     t.string  "password_digest"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
